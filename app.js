f167103a748b984724ceba198c734b2dfc1dd5b2
Muneshkumar12 <munesh788mk@gmail.com>
2023-09-25 16:19:00 +0500

    simple form in js

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ let lastName = document.getElementById("last-name");
 let password = document.getElementById("password");
 let confPassword = document.getElementById("conf-password");
 let paragraph = document.getElementById("para");
+let email = document.getElementById("email");
+let regex = /^[\w\-\.\+]+\@[a-zA-Z0-9\. \-]+\.[a-zA-z0-9]{2,4}$/;
+var term = document.getElementById("term");
 let users = [];
 
 addData = () => {
@@ -19,6 +22,22 @@ addData = () => {
         paragraph.innerHTML = "Full Name required!"
         paragraph.style.color = "red";
         lastName.focus();
+        setTimeout(() => {
+            paragraph.style.display = "none"
+        }, 1000);
+    } else if (email.value === "") {
+        paragraph.style.display = "block"
+        paragraph.innerHTML = "Email Address required!"
+        paragraph.style.color = "red";
+        email.focus();
+        setTimeout(() => {
+            paragraph.style.display = "none"
+        }, 1000);
+    } else if (!email.value.match(regex)) {
+        paragraph.style.display = "block"
+        paragraph.innerHTML = "please enter invalid email!"
+        paragraph.style.color = "red";
+        email.focus();
         setTimeout(() => {
             paragraph.style.display = "none"
         }, 1000);
@@ -54,22 +73,32 @@ addData = () => {
         setTimeout(() => {
             paragraph.style.display = "none"
         }, 1000);
-    } else {
+    } else if(term.checked === false){
+        paragraph.style.display = "block";
+        paragraph.style.color = "red";
+        paragraph.innerHTML = "please select term and policy!"
+        setTimeout(() => {
+        paragraph.style.display = "none";
+        }, 1000);
+    }else {
         paragraph.style.display = "block";
         paragraph.style.color = "green";
         paragraph.innerHTML = "Your form submit successfully."
         setTimeout(() => {
-        paragraph.style.display = "none";
-        name.value = "";
-        lastName.value = "";
-        password.value = "";
-        confPassword.value = "";
+            paragraph.style.display = "none";
+            name.value = "";
+            lastName.value = "";
+            email.value = "";
+            password.value = "";
+            confPassword.value = "";
+            term.checked = ""
         }, 1000);
         obj = {
             name: name.value,
             lastName: lastName.value,
             password: password.value,
             confirmPass: confPassword.value,
+            term: term.checked,
         }
         users.push(obj)
         console.log(users)
